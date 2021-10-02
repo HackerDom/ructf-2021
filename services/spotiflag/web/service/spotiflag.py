e8646e195530a63a22d31b997f41b73c9510330b
Vladimir <keltecc@ya.ru>
2021-10-02 19:26:05 +0500

    Revert "spotiflag: wait for spotiflag"
    This reverts commit 411c3d16973ae2742ab911f577ddd8a1e3ad3271.

--- services/spotiflag/web/service/spotiflag.py
+++ services/spotiflag/web/service/spotiflag.py
@@ -36,8 +36,6 @@ class Spotiflag:
             writer.write(description)
             await writer.drain()
 
-            await writer.wait_closed()
-
     @staticmethod
     async def read(id: uuid.UUID, offset: int):
         async with Spotiflag.connect_with_id(id) as (reader, writer):
