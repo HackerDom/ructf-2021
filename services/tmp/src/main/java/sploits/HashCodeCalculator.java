18d27c2b24669983b1c65f4df72346b3798158ca
rx00 <rx00@hackerdom.ru>
2021-10-02 18:24:08 +0500

    cleanup

--- services/tmp/src/main/java/sploits/HashCodeCalculator.java
+++
@@ -1,7 +0,0 @@
-package sploits;
-
-public class HashCodeCalculator {
-    public static long calculateHash(String device, String type) {
-        return (long) device.hashCode() + type.hashCode();
-    }
-}
