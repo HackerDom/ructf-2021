1f8a70a900518ede44ef7517eb6aab4ecd91f29e
rkhapov <rkhapov@yandex-team.ru>
2021-10-03 03:32:35 +0500

    wip

--- checkers/trackbattle/xss_runner/app.py
+++ checkers/trackbattle/xss_runner/app.py
@@ -355,7 +355,7 @@ def run_playing():
 
 
 threading.Thread(target=remove_old_users).start()
-# threading.Thread(target=run_playing).start()
+threading.Thread(target=run_playing).start()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=31337, debug=True)
