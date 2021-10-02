425870f40d884a4d8429214e0522fb35445022bb
rx00 <rx00@hackerdom.ru>
2021-10-02 18:22:24 +0500

    rm

--- services/Metrics/src/main/java/ru/ctf/exceptions/InvalidJsonException.java
+++
@@ -1,7 +0,0 @@
-package ru.ctf.exceptions;
-
-public class InvalidJsonException extends Exception {
-    public InvalidJsonException(String message) {
-        super(message);
-    }
-}
--- services/Metrics/src/main/java/ru/ctf/services/SerializationService.java
+++
@@ -1,7 +0,0 @@
-package ru.ctf.services;
-
-public interface SerializationService<Entity, Source> {
-    Source serialize(Entity obj);
-
-    Entity deserialize(Source source);
-}
