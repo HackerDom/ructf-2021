ab1d30f6609e5397e6ae8ad74f9ee06892ac0789
dvl <dvl@darktree.com>
2021-10-02 20:24:39 +0500

    cleanup

--- services/white_album/rebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker-compose -f docker-compose-build.yml down
-docker-compose down
-
-docker system prune --all -f
-
-docker-compose -f docker-compose-build.yml up --build --remove-orphans --force-recreate
-docker-compose up --build --force-recreate --remove-orphans
