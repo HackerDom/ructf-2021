ea8179b4b53ba0ab079374ebe26130e874d52894
rkhapov <rkhapov@yandex-team.ru>
2021-10-03 03:23:29 +0500

    some fixes

--- services/trackbattle/src/front/src/Utilities/HashConverter.ts
+++ services/trackbattle/src/front/src/Utilities/HashConverter.ts
@@ -1,9 +1,7 @@
+import {SHA256} from 'crypto-ts';
+
 export async function sha256(message: string): Promise<string> {
-    const msgBuffer = new TextEncoder().encode(message);
-    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-    return hashHex;
+    return SHA256(message);
 }
 
 export function toBase64(message: string): string {
