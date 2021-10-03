37adc13282ee667091811ce2ee822ba869abae25
vaspahomov <vas2142553@gmail.com>
2021-10-03 16:47:47 +0500

    improve shit generator

--- services/studio/checker_payload/generate_dummy_code.py
+++ services/studio/checker_payload/generate_dummy_code.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 import random
 import string
 
@@ -20,6 +19,15 @@ def define_int():
     n = random.randint(0, 1000)
     return f'int {s} = {n};', s
 
+def generate_flag():
+    flag_symbols = string.ascii_uppercase + string.digits
+    return "".join((flag_symbols[random.randint(0, len(flag_symbols)-1)] for _ in range(31))) + "="
+
+def define_str():
+    s = ''.join(random.choices(string.ascii_lowercase, k=10))
+    val = generate_flag()
+    return f'string {s} = "{val}";', val
+
 def generate_shit():
     code = ''
     for i in range(random.randint(5, 20)):
@@ -30,7 +38,21 @@ def generate_shit():
             f = rand_op(name, random.randint(5, 20))
             ops += '\t' + f + '\n'
         code += rand_cond(ops) + '\n'
+
+    strs_count = random.randint(5, 10)
+    ops = []
+    for i in range(strs_count):
+        op, name = define_str()
+        ops.append(op)
+    
+    #flag_var_name = "".join(random.choices(string.ascii_lowercase, k=10))
+    #ops[random.randint(0, strs_count)] = f'string {flag_var_name} = "{flag}";'
+
+    code += '\n'.join(ops)
+
     return code
 
-print(generate_shit())
 
+code = generate_shit()
+
+print(code)
