eabc0a9652afb66b3838fb42b896e6b1d7d5f986
usernamedt <usernamedt@protonmail.ch>
2021-10-03 15:23:14 +0300

    update checker

--- checkers/studio/payload_generator.py
+++ checkers/studio/payload_generator.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import os
+import random 
 
 FLAG_OFFSET = 2712
-
+TARGET = './generated/'
 
 def get(flag):
     error = ""
@@ -18,10 +19,15 @@ def __get(flag):
     res = bytearray()
     dir_path = os.path.dirname(os.path.realpath(__file__))
 
-    with open(os.path.join(dir_path, 'checker_payload'), 'rb') as f:
+    files = os.listdir(TARGET)
+    payload_name = random.choice(files)
+
+    with open(os.path.join(dir_path, TARGET + payload_name), 'rb') as f:
+        _, offset = payload_name.split('_')
+        offset = int(offset)
         content = bytearray(f.read())
-        res.extend(content[:FLAG_OFFSET])
+        res.extend(content[:offset])
         enc_flag = flag.encode()
         res.extend(bytearray(enc_flag))
-        res.extend(content[FLAG_OFFSET+32:])
+        res.extend(content[offset+32:])
     return res
