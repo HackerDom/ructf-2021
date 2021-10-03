fa7d64c7b5859d303deea71a2dca8b60642a4795
usernamedt <usernamedt@protonmail.ch>
2021-10-03 14:44:57 +0300

    add generator

---
+++ services/studio/checker_payload/gen_binaries.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+DST_FOLDER=generated
+mkdir -p $DST_FOLDER
+
+for i in `seq 1 $1`
+do
+  bash ./build.sh
+  OFFSET=$(grep -a -b 'PLACEHOLDERREPLACEMEWITHREALFLAG' checker_payload | awk -F ':' '{print $1}')
+  mv checker_payload $DST_FOLDER/$i"_"$OFFSET 
+done
+
